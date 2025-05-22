bfcc56f7dcdf2560020c33218e57fcbd4113589e
Angel <angeloxv@outlook.it>
2025-05-22 21:11:43 +0200

    references page

---
+++ src/components/references/data.js
@@ -0,0 +1,11 @@
+const references = [
+  {
+    userName: "Robert Bye",
+    userLink:
+      "https://unsplash.com/it/@robertbye?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
+    imageLink:
+      "https://unsplash.com/it/foto/uomo-che-indossa-giacca-blu-in-piedi-sulla-collina-di-fronte-a-unaltra-collina-HTzGwHaN7VA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
+  },
+];
+
+export default references;
